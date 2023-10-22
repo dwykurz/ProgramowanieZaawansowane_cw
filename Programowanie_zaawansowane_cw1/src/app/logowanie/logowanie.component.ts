0b8609ad85a6fb80c851151082ac75cf12279210
dwykurz <dawid.wykurz@gmail.com>
2023-10-22 14:03:08 +0200

    Poprawa błędów

--- Programowanie_zaawansowane_cw1/src/app/logowanie/logowanie.component.ts
+++ Programowanie_zaawansowane_cw1/src/app/logowanie/logowanie.component.ts
@@ -1,21 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-logowanie',
   templateUrl: './logowanie.component.html',
-  styleUrls: ['./logowanie.component.css']
+  styleUrls: ['./logowanie.component.scss']
 })
-export class LogowanieComponent {
+export class LogowanieComponent implements OnInit {
+
   formData = {
     username: '',
     password: ''
-  };
+  }
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
 
   logowanie() {
-    if (this.formData.username === 'przykladowyUzytkownik' && this.formData.password === 'przykladoweHaslo') {
+    if(this.formData.username === 'przykladowyUzytkownik' && this.formData.password === 'przykladoweHaslo') {
       console.log('Zalogowano pomyślnie');
     } else {
-      console.log('Błąd logowania. Sprawdź dane.');
+      console.log('Błąd logowanie. Sprawdź dane.');
     }
   }
 
